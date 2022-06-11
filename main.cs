b02701113d03b85bb77763a78d21dd43696b41a6
RNAV 2019 <50592341+RNAV2019@users.noreply.github.com>
2022-06-11 10:23:34 +0000

    Finished adding features

--- main.cs
+++ main.cs
@@ -2,11 +2,50 @@ using System;
 using System.Threading;
 using System.IO;
 using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
 
 public class Program
 {
+    public class RowComparer : IComparer<string>
+    {
+        private int _columnIndex;
+
+        public RowComparer(int columnIndex)
+        {
+            _columnIndex = columnIndex;
+
+        }
+        public int Compare(string a, string b)
+        {
+            string valA = a.Split(',')[_columnIndex];
+            string valB = b.Split(',')[_columnIndex];
+            if (valA == valB) return 0;
+
+            return int.Parse(valA) > int.Parse(valB) ? 1 : -1;
+
+        }
+    }
+
+    public class User
+    {
+        public string username { get; set; }
+        public int score { get; set; }
+    }
+
+    public sealed class UserMap : ClassMap<User>
+    {
+        public UserMap()
+        {
+            Map(m => m.username).Name("Username");
+            Map(m => m.score).Name("Score");
+        }
+    }
     public static void Main(string[] args)
     {
+        // Variables
         bool playAgain = true;
         int pauseDuration = 500;
         // Initialise arrays
@@ -49,7 +88,7 @@ public class Program
           "program counter"
         };
 
-        // Length of array
+        // Check whether a user is a new user or an existing user.
         int lenOfQuestions = questions.Length;
         while (playAgain)
         {
@@ -96,27 +135,10 @@ public class Program
                 }
             }
 
-            /*
-            var fileContents = File.ReadLines("logins.txt");
-            foreach (string line in fileContents)
-            {
-                string[] columns = line.Trim('\n').Split(' ');
-                string username = columns[0];
-                string password = columns[1];
-
-                usernames.Add(username);
-                passwords.Add(password);
-            }
-            */
-
-            /*
-            usernames.ForEach(i => Console.Write("{0}\n", i));
-            passwords.ForEach(i => Console.Write("{0}\n", i));
-            */
-
             string currUser = "";
             string userPassword = "";
 
+            // Verify the users credentials when making a new account.
             if (isNewUser)
             {
                 bool passedVerification = false;
@@ -146,36 +168,47 @@ public class Program
                         currUser = username;
                         userPassword = password;
                     }
+                    // Append the login details to a text file.
                     using (StreamWriter sw = File.AppendText("logins.txt"))
                     {
                         sw.WriteLine($"{currUser}={userPassword}");
                     }
                 }
-            } else {
-              bool loginSuccess = false;
+            }
+            else
+            {
+                bool loginSuccess = false;
 
-              while(loginSuccess == false) {
-                Console.Write("Please enter your username: ");
-                string username = Console.ReadLine();
+                while (loginSuccess == false)
+                {
+                    // Logging in with username and password if user exists in text file
+                    Console.Write("Please enter your username: ");
+                    string username = Console.ReadLine();
 
-                Console.Write("Please enter your password: ");
-                string password = Console.ReadLine();
+                    Console.Write("Please enter your password: ");
+                    string password = Console.ReadLine();
 
-                for (int i = 0; i < usernames.Count; i++) {
-                  if (usernames[i] == username && passwords[i] == password) {
-                    loginSuccess = true;
-                    currUser = username;
-                    userPassword = password;
-                  }
+                    for (int i = 0; i < usernames.Count; i++)
+                    {
+                        if (usernames[i] == username && passwords[i] == password)
+                        {
+                            loginSuccess = true;
+                            currUser = username;
+                            userPassword = password;
+                        }
+                    }
+                    if (loginSuccess)
+                    {
+                        Console.WriteLine($"Login Success. You are now logged in as {username} \n");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Login failed. Please check your login details and try again.");
+                    }
                 }
-                if (loginSuccess) {
-                  Console.WriteLine($"Login Success. You are now logged in as {username} \n");
-                } else {
-                  Console.WriteLine($"Login failed. Please check your login details and try again.");
-                }
-              }
             }
 
+            // Start of quiz
             Console.WriteLine($"Welcome {currUser} to the GCSE Computer Science OCR quiz.");
             Console.WriteLine("You will be tested on 10 questions from the OCR GCSE Computer Science course.\n");
             Thread.Sleep(pauseDuration);
@@ -185,7 +218,7 @@ public class Program
 
             // Loop through questions
             for (int i = 1; i < lenOfQuestions + 1; i++)
-            {  
+            {
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine($"QUESTION {i}:\n");
                 Console.ForegroundColor = ConsoleColor.Cyan;
@@ -209,8 +242,10 @@ public class Program
                 }
                 Thread.Sleep(pauseDuration);
             };
+            // Quiz is complete
             Console.WriteLine("The quiz is complete.");
             Console.WriteLine($"Your final score is {score} out of {lenOfQuestions}");
+            // Anaylse the users score.
             if (score < 5)
             {
                 Console.WriteLine("Better luck next time!");
@@ -227,13 +262,65 @@ public class Program
             {
                 Console.WriteLine("Full marks!");
             }
+
+            // Appends username and score to csv file.
+            var csvPath = File.Open("scores.csv", FileMode.Append);
+            using (var streamWriter = new StreamWriter(csvPath))
+            {
+                var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    // Don't write the header again.
+                    HasHeaderRecord = false,
+                };
+
+                using (var csvWriter = new CsvWriter(streamWriter, config))
+                {
+                    csvWriter.WriteField(currUser);
+                    csvWriter.WriteField(score);
+                    csvWriter.NextRecord();
+                }
+            }
+
+            // Sorting CSV File
+            List<string> rows = File.ReadAllLines("scores.csv").ToList();
+            rows = rows.Skip(1).OrderByDescending(r => r, new RowComparer(1)).ToList();
+
+            File.WriteAllText("scores.csv", "");
+            // var csvPath = File.Open("scores.csv", FileMode.Append);
+            using (StreamWriter sw = File.AppendText("scores.csv"))
+            {
+                sw.WriteLine("Username,Score");
+            }
+            File.AppendAllLines("scores.csv", rows);
+
+            // Console.WriteLine the scores to the console.
+            using (var reader = new StreamReader("scores.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                csv.Context.RegisterClassMap<UserMap>();
+                var users = csv.GetRecords<User>();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Leaderboards: ");
+                foreach (var user in users.Take(3))
+                {
+                    Console.WriteLine($"{user.username} - {user.score}");
+                }
+                Console.ResetColor();
+            }
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.Write("\nWould you like to play again (yes or no)?: ");
+            Console.ResetColor();
             string nextGame = Console.ReadLine();
-            if (nextGame.ToLower().Trim() == "yes") {
-              Console.Clear();
-            } else {
-              Console.WriteLine("Thanks for playing!");
-              playAgain = false;
+            if (nextGame.ToLower().Trim() == "yes")
+            {
+                Console.Clear();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Thanks for playing!");
+                Console.ResetColor();
+                playAgain = false;
             }
         }
     }
